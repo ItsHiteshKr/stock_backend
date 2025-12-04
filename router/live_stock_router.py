83d2453021bbbf5fff531caa8e01eb69382c10c2
Yukti Chaurasia <chaurasiayukti29@gmail.com>
2025-12-04 14:07:42 +0530

    Update live stock API to use yfinance instead of NSE

--- router/live_stock_router.py
+++ router/live_stock_router.py
@@ -1,17 +1,13 @@
 from fastapi import APIRouter, HTTPException
 from schema.live_stock_schema import StockPriceResponse
-from service.live_stock_service import get_live_nse_price
+from service.live_stock_service import get_live_yf_price  # updated
 
 router = APIRouter()
 
 @router.get("/stocks/price/{symbol}", response_model=StockPriceResponse)
 def live_stock_price(symbol: str):
-    """
-    Get live price of NSE symbol safely.
-    """
     try:
-        data = get_live_nse_price(symbol.upper())
+        data = get_live_yf_price(symbol.upper())
         return data
     except Exception as e:
-        # Return clear error to the API user
-        raise HTTPException(status_code=500, detail=f"Error fetching {symbol}: {str(e)}")
+        raise HTTPException(status_code=500, detail=str(e))
